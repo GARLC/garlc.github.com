afb9f5e59a72815d22fafa8f7aef90280cab9d6f
Kaleb Hornsby <kaleb@horns.by>
2013-01-06 18:39:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ $(function(){
     // Social //
     ////////////
     $('.follow[title]').tooltip();
+    //User Voice
+    $('.follow .icon-envelope-alt').parents('a').on('click', function(e){
+        UserVoice.showPopupWidget();
+        e.preventDefault();
+    })
+    //Twitter
     $.getJSON('http://api.twitter.com/1/users/show.json?screen_name=GeorgiaRLC&callback=?', function(data){
         $('.follow .icon-twitter').parents('li')
         .attr('data-original-title', data.followers_count + ' followers on Twitter');
     });
+    //Facebook
     $.getJSON("https://graph.facebook.com/GeorgiaRLC?callback=?", function(data){
         $('.follow .icon-facebook').parents('li')
         .attr('data-original-title', data.likes + ' likes on Facebook');
